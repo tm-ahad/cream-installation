1ff395bec37aeaed7792a282c01c48129b0d0cc0
tm-ahad <tm.ahad.07@gmail.com>
2023-08-17 21:44:05 +0600

    Support for darwin added.

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.1.0/cream -o ./cream
+if [[ "$OSTYPE" == "linux-gnu" ]]
+then
+    curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.1.0-linux-alpha/cream -o ./cream
+elif [[ "$OSTYPE" == "darwin"* ]]
+then
+    curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.1.0-darwin/cream -o ./cream
+fi
 sudo chmod 777 ./cream
 sudo mv ./cream /usr/bin/cream
+echo -e "\e[1;32mCream has successfully installed on your machine.\e[0m"
+
