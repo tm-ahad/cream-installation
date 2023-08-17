7212f90e450f5a26458eb8d3f1382f5508bbc97f
tm-ahad <tm.ahad.07@gmail.com>
2023-08-17 22:27:54 +0600

    Bug fixed.

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-
 if [[ "$OSTYPE" == "linux-gnu" ]]
 then
     curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.1.0-linux-alpha/cream -o ./cream
+    sudo chmod 777 ./cream
+    sudo mv ./cream /usr/bin/cream
 elif [[ "$OSTYPE" == "darwin"* ]]
 then
     curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.1.0-darwin/cream -o ./cream
+    sudo chmod 777 ./cream
+    sudo mv ./cream /usr/local/bin/cream
 fi
-sudo chmod 777 ./cream
-sudo mv ./cream /usr/bin/cream
+
 echo -e "\e[1;32mCream has successfully installed on your machine.\e[0m"
 
