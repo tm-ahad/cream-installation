687876e39f8fa66f8bcfe91d49a8477dc9ea41f8
tm-ahad <tm.ahad.07@gmail.com>
2023-12-31 19:30:58 +0600

    Installation and update.sh updated

--- update.bash
+++ update.bash
@@ -1,16 +1,16 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]
 then
-    curl -L0 https://github.com/tm-ahad/cream/releases/download/v0.4.2-linux/cream -o ./cream
+    curl -L0 https://github.com/tm-ahad/cream/releases/download/v0.4.2-alpha/linux -o ./cream
     sudo chmod 777 ./cream
     sudo mv ./cream /usr/bin/cream
 elif [[ "$OSTYPE" == "darwin"* ]]
 then
-    curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.4.2-darwin/cream -o ./cream
+    curl -L0 https://github.com/tm-ahad/cream/releases/download/v0.4.2-alpha/darwin -o ./cream
     sudo chmod 777 ./cream
     sudo mv ./cream /usr/local/bin/cream
 elif [[ "$OSTYPE" == "msys" ]]
 then
-    curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.4.2-msys/cream.exe -o ./cream.exe
+    curl -L0 https://www.github.com/tm-ahad/cream/releases/download/v0.4.2-alpha/windows.exe -o ./cream.exe
 
     CH=$HOME/.cream
     BASHRC=$HOME/.bashrc
